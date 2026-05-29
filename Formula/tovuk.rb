3573406c7f599675afdf1d73e6a1dbb659d3f4dd
kriptoburak <kriptoburak@users.noreply.github.com>
2026-05-29 21:48:48 +0300

    Move CLI command policy out of frontend checks

--- Formula/tovuk.rb
+++ Formula/tovuk.rb
@@ -1,7 +1,7 @@
 class Tovuk < Formula
   desc "Deploy Rust backends, static frontends, and fullstack apps to Tovuk"
   homepage "https://tovuk.com"
-  url "https://github.com/tovuk/tovuk.git", tag: "v0.1.54"
+  url "https://github.com/tovuk/tovuk.git", tag: "v0.1.55"
   license "MIT"
 
   depends_on "rust" => :build
