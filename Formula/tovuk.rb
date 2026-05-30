a38e53735cc932a0026953b5535dc534180d01bb
kriptoburak <kriptoburak@users.noreply.github.com>
2026-05-30 03:05:08 +0300

    Scope storage CLI options

--- Formula/tovuk.rb
+++ Formula/tovuk.rb
@@ -1,7 +1,7 @@
 class Tovuk < Formula
   desc "Deploy Rust backends, static frontends, and fullstack apps to Tovuk"
   homepage "https://tovuk.com"
-  url "https://github.com/tovuk/tovuk.git", tag: "v0.1.59"
+  url "https://github.com/tovuk/tovuk.git", tag: "v0.1.60"
   license "MIT"
 
   depends_on "rust" => :build
