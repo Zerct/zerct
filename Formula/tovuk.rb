58b016ea7c59c2e5768e1ca869714a5e0224c186
kriptoburak <kriptoburak@users.noreply.github.com>
2026-07-05 21:27:10 +0300

    Bump public package version to 0.1.115

--- Formula/tovuk.rb
+++ Formula/tovuk.rb
@@ -1,7 +1,7 @@
 class Tovuk < Formula
   desc "Use Tovuk scraper APIs from a native CLI"
   homepage "https://tovuk.com"
-  url "https://github.com/tovuk/tovuk.git", tag: "v0.1.114"
+  url "https://github.com/tovuk/tovuk.git", tag: "v0.1.115"
   license "MIT"
 
   depends_on "rust" => :build
